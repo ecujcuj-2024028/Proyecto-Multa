6ba6286eaaa1fb2baf6abec86e3b930b8456b9bf
ecujcuj-2024028 <ecujcuj-2024028@kinal.edu.gt>
2025-10-29 17:40:16 -0600

    feat: feat: Implementa Menú Principal y CRUD de Municipios con MVC y conexion a la base de datos

---
+++ AppProyectoMulta/View/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using AppProyectoMulta.View;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppProyectoMulta
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Multas_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Municipios_Click(object sender, RoutedEventArgs e)
+        {
+            GestionMunicipios ventanaMunicipios = new GestionMunicipios();
+
+            ventanaMunicipios.Show();
+            this.Close();
+        }
+
+        private void Dueños_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Vehiculos_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Agentes_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TiposMulta_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Pagos_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
