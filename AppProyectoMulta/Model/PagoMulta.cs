dab8760fd37388e535c001c797c63741427d3996
ecujcuj-2024028 <ecujcuj-2024028@kinal.edu.gt>
2025-11-03 15:57:21 -0600

    feat: implementar gestión de Pago Multa con CRUD completo

---
+++ AppProyectoMulta/Model/PagoMulta.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AppProyectoMulta.Model
+{
+    public class PagoMulta
+    {
+        public int IdPago { get; set; }
+        public int IdMulta { get; set; }
+        public DateTime FechaPago { get; set; }
+        public string MetodoPago { get; set; }
+        public decimal MontoPagado { get; set; }
+    }
+}
